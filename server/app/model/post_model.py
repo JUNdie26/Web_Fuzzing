53fa7db328334b07d730ba5eda4baae8666e1b9a
lazyduck34929 <112916656+lazyduck34929@users.noreply.github.com>
2025-11-15 18:14:56 +0900

    Model file for post function
    Model file for post function

--- server/app/model/post_model.py
+++ server/app/model/post_model.py
@@ -4,10 +4,10 @@ from datetime import datetime
 db = SQLAlchemy()
 
 class Post(db.Model):
-    __tablename__ = 'posts'
+    __tablename__ = "posts"
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(200), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    author_id = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
